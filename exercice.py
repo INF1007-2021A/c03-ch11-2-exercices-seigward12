af64e86c6cd94c8607ab1755d05d0d5f07c0859a
seigward12 <alexis.nicolas@polymtl.ca>
2021-11-23 10:40:45 -0500

    update

--- exercice.py
+++ exercice.py
@@ -10,7 +10,12 @@ from game import *
 
 
 def simulate_battle():
-	pass
+	ch1 = Character('mico', 100, 5, 10, 15)
+	ch2 = Magician('Jacques', 100, 35, 3, 15, 7, 17)
+
+	while ch1.hp > 0 or ch2.hp > 0:
+		print('yo')
+
 
 
 def main():
